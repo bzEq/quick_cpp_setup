27314d32b54c083b252a7a550bbb62629be27186
Kai Luo <gluokai@gmail.com>
2018-01-16 00:30:17 +0800

    Add LICENSE

--- quick_cpp_setup/layout.py
+++ quick_cpp_setup/layout.py
@@ -5,6 +5,7 @@ import os
 import sys
 import logging
 import string
+import datetime
 
 gtest_BUILD = """\
 cc_library(
@@ -65,6 +66,37 @@ YAPF = """\
 [style]
 based_on_style = pep8
 """
+
+BSD = string.Template("""\
+Copyright (c) $year $author. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+""")
 
 
 class Builder(object):
@@ -128,4 +160,14 @@ class Builder(object):
             ok = self.add_yapf()
             if not ok:
                 return False
+        self.add_bsd()
         return True
+
+    def add_bsd(self):
+        with open(os.path.join(self.project_root, "LICENSE"), "w") as f:
+            now = datetime.datetime.now()
+            content = BSD.substitute({
+                "year": str(now.year),
+                "author": self.configuration.author,
+            })
+            f.write(content)
